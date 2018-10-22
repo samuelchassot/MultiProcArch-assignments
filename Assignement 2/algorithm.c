93bf5e0a83b9d144537e2caafdc7753332031bd7
Simon Wicky <simon.wicky@epfl.ch>
2018-10-22 17:03:40 +0200

    speed up by splitting into 4, plus sample output

--- Assignement 2/algorithm.c
+++ Assignement 2/algorithm.c
@@ -1,8 +1,8 @@
 /*
 ============================================================================
 Filename    : algorithm.c
-Author      : Your names go here
-SCIPER      : Your SCIPER numbers
+Author      : Samuel Chassot and Simon Wicky
+SCIPER      : 270955 - 260589
 
 ============================================================================
 */
@@ -18,17 +18,22 @@ void simulate(double *input, double *output, int threads, int length, int iterat
     // Parallelize this!!
     for(int n=0; n < iterations; n++)
     {
-        for(int i=1; i<length-1; i++)
+        for(int i=1; i<length / 2; i++)
         {
-            for(int j=1; j<length-1; j++)
+            for(int j=1; j<length / 2; j++)
             {
-                    if ( ((i == length/2-1) || (i== length/2))
-                        && ((j == length/2-1) || (j == length/2)) )
+                    if ( (i == length/2-1)
+                        && (j == length/2-1))
                         continue;
 
-                    OUTPUT(i,j) = (INPUT(i-1,j-1) + INPUT(i-1,j) + INPUT(i-1,j+1) +
+                    double temp2 = (INPUT(i-1,j-1) + INPUT(i-1,j) + INPUT(i-1,j+1) +
                                    INPUT(i,j-1)   + INPUT(i,j)   + INPUT(i,j+1)   +
                                    INPUT(i+1,j-1) + INPUT(i+1,j) + INPUT(i+1,j+1) )/9;
+                    OUTPUT(i,j) = temp2;
+                    OUTPUT(length-1-i,j) = temp2;
+                    OUTPUT(i,length-1-j) = temp2;
+                    OUTPUT(length-1-i,length-1-j) = temp2;
+
             }
         }
 
