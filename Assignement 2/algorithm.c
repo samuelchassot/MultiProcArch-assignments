05eff097c2a00e892acc29925c2dfae749b582bb
samuelchassot <samuel.chassot@epfl.ch>
2018-10-31 15:17:21 +0100

    parallelization of the last write operation

--- Assignement 2/algorithm.c
+++ Assignement 2/algorithm.c
@@ -57,13 +57,44 @@ void simulate(double *input, double *output, int threads, int length, int iterat
         input = output;
         output = temp;
     }
-
-    for(int i = 0 ; i < length/2 ; ++i){
-        for(int j = 0 ; j < length/2 ; ++j){
-            double cur = OUTPUT(i, j);
-            OUTPUT(length-1-i, j) = cur;
-            OUTPUT(i, length-1-j) = cur;
-            OUTPUT(length-1-i, length-1-j) = cur;
+    
+    if(get_num_threads() < 3){
+        for(int i = 0 ; i < length/2 ; ++i){
+            for(int j = 0 ; j < length/2 ; ++j){
+                double cur = OUTPUT(i, j);
+                OUTPUT(length-1-i, j) = cur;
+                OUTPUT(i, length-1-j) = cur;
+                OUTPUT(length-1-i, length-1-j) = cur;
+            }
         }
     }
+    else{
+        if(omp_get_thread_num() == 0){
+            for(int i = 0 ; i < length/2 ; ++i){
+                for(int j = 0 ; j < length/2 ; ++j){
+                    double cur = OUTPUT(i, j);
+                    OUTPUT(length-1-i, j) = cur;
+                }
+            }
+        }
+        if(omp_get_thread_num() == 1){
+            for(int i = 0 ; i < length/2 ; ++i){
+                for(int j = 0 ; j < length/2 ; ++j){
+                    double cur = OUTPUT(i, j);
+                    OUTPUT(i, length-1-j) = cur;
+                }
+            }
+        }
+
+        if(omp_get_thread_num() == 2){
+            for(int i = 0 ; i < length/2 ; ++i){
+                for(int j = 0 ; j < length/2 ; ++j){
+                    double cur = OUTPUT(i, j);
+                }
+                    OUTPUT(length-1-i, length-1-j) = cur;
+            }
+        }
+    }
+
+    
 }
--- Assignement 2/populate_function.c
+++ Assignement 2/populate_function.c
@@ -4,12 +4,29 @@
 void simulate(double *input, double *output, int threads, int length, int iterations)
 {
     void populate(){
-        for(int i = 0 ; i < length/2 ; ++i){
-            for(int j = 0 ; j < length/2 ; ++j){
-                double cur = OUTPUT(i, j);
-                OUTPUT(length-1-i, j) = cur;
-                OUTPUT(i, length-1-j) = cur;
-                OUTPUT(length-1-i, length-1-j);
+        if(omp_get_thread_num() == 0){
+            for(int i = 0 ; i < length/2 ; ++i){
+                for(int j = 0 ; j < length/2 ; ++j){
+                    double cur = OUTPUT(i, j);
+                    OUTPUT(length-1-i, j) = cur;
+                }
+            }
+        }
+        if(omp_get_thread_num() == 1){
+            for(int i = 0 ; i < length/2 ; ++i){
+                for(int j = 0 ; j < length/2 ; ++j){
+                    double cur = OUTPUT(i, j);
+                    OUTPUT(i, length-1-j) = cur;
+                }
+            }
+        }
+
+        if(omp_get_thread_num() == 1){
+            for(int i = 0 ; i < length/2 ; ++i){
+                for(int j = 0 ; j < length/2 ; ++j){
+                    double cur = OUTPUT(i, j);
+                }
+                    OUTPUT(length-1-i, length-1-j) = cur;
             }
         }
     }
