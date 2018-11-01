926b9a930a2f59037a561d286c3541ec41c641de
Simon Wicky <simon.wicky@epfl.ch>
2018-11-01 11:02:51 +0100

    test2

--- Assignement 2/algorithm.c
+++ Assignement 2/algorithm.c
@@ -32,38 +32,42 @@ void simulate(double *input, double *output, int threads, int length, int iterat
     // Parallelize this!!
     for(int n=0; n < iterations; n++) {
 
-        //#pragma omp parallel for
-        for(int i=1; i<length / 2; i++) {
-            //#pragma omp parallel for
+        #pragma omp parallel{
 
-            for(int j=1; j<length / 2; j++) {
+    		#pragma omp for
+	        for(int i=1; i<length / 2; i++) {
+	            
+	            #pragma omp for
+	            for(int j=1; j<length / 2; j++) {
 
-                    if ( (i == midIndex) && (j == midIndex)){
-                        continue;
-                    }
-                    double temp2;
+	                    if ( (i == midIndex) && (j == midIndex)){
+	                        continue;
+	                    }
+	                    double temp2;
 
-                    if (j == midIndex){
-                    	temp2 = (PINPUT(i-1,j-1) + 2 * PINPUT(i-1,j) +
-                                   PINPUT(i,j-1)   + 2 * PINPUT(i,j)   +
-                                   PINPUT(i+1,j-1) + 2 * PINPUT(i+1,j) )/9;
-                    }
-                    else if (i == midIndex){
-                    	temp2 = (PINPUT(i-1,j-1) + PINPUT(i-1,j) + PINPUT(i-1,j+1) +
-                                   2 *PINPUT(i,j-1)   + 2 *PINPUT(i,j)   + 2 *PINPUT(i,j+1) )/9;
-                    }
-                    else {
-	                    temp2 = (PINPUT(i-1,j-1) + PINPUT(i-1,j) + PINPUT(i-1,j+1) +
-	                                   PINPUT(i,j-1)   + PINPUT(i,j)   + PINPUT(i,j+1)   +
-	                                   PINPUT(i+1,j-1) + PINPUT(i+1,j) + PINPUT(i+1,j+1) )/9;                    	
-                    }
+	                    if (j == midIndex){
+	                    	temp2 = (PINPUT(i-1,j-1) + 2 * PINPUT(i-1,j) +
+	                                   PINPUT(i,j-1)   + 2 * PINPUT(i,j)   +
+	                                   PINPUT(i+1,j-1) + 2 * PINPUT(i+1,j) )/9;
+	                    }
+	                    else if (i == midIndex){
+	                    	temp2 = (PINPUT(i-1,j-1) + PINPUT(i-1,j) + PINPUT(i-1,j+1) +
+	                                   2 *PINPUT(i,j-1)   + 2 *PINPUT(i,j)   + 2 *PINPUT(i,j+1) )/9;
+	                    }
+	                    else {
+		                    temp2 = (PINPUT(i-1,j-1) + PINPUT(i-1,j) + PINPUT(i-1,j+1) +
+		                                   PINPUT(i,j-1)   + PINPUT(i,j)   + PINPUT(i,j+1)   +
+		                                   PINPUT(i+1,j-1) + PINPUT(i+1,j) + PINPUT(i+1,j+1) )/9;                    	
+	                    }
 
-                     POUTPUT(i,j) = temp2;
+	                     POUTPUT(i,j) = temp2;
 
 
 
-            }
-        }
+	            }
+	        }
+
+    	}
 
         if (n != iterations -1){
                 temp = pInput;
