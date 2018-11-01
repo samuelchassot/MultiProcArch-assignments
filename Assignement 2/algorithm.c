36e3df5321043ed8812163049b12bfedb7399805
Simon Wicky <simon.wicky@epfl.ch>
2018-11-01 13:57:08 +0100

    remove problematic padding

--- Assignement 2/algorithm.c
+++ Assignement 2/algorithm.c
@@ -11,25 +11,12 @@ SCIPER      : 270955 - 260589
 #define INPUT(I,J) input[(I)*length+(J)]
 #define OUTPUT(I,J) output[(I)*length+(J)]
 
-#define PINPUT(I,J) pInput[8*((I)*length+(J))]
-#define POUTPUT(I,J) pOutput[8*((I)*length+(J))]
 
 
 void simulate(double *input, double *output, int threads, int length, int iterations) {
     double *temp;
     double midIndex = length / 2 - 1;
-
-    double *pInput = calloc(8*length*length, sizeof(double));
-    double *pOutput = calloc(8*length*length, sizeof(double));
-    for(int i=1; i<length / 2; i++) {
-        for(int j=1; j<length / 2; j++) {
-            PINPUT(i,j) = INPUT(i,j);
-            POUTPUT(i,j) = OUTPUT(i,j);
-        }
-    }
-
     
-    // Parallelize this!!
     for(int n=0; n < iterations; n++) {
 
         #pragma omp parallel 
@@ -47,21 +34,21 @@ void simulate(double *input, double *output, int threads, int length, int iterat
 	                    double temp2;
 
 	                    if (j == midIndex){
-	                    	temp2 = (PINPUT(i-1,j-1) + 2 * PINPUT(i-1,j) +
-	                                   PINPUT(i,j-1)   + 2 * PINPUT(i,j)   +
-	                                   PINPUT(i+1,j-1) + 2 * PINPUT(i+1,j) )/9;
+	                    	temp2 = (INPUT(i-1,j-1) + 2 * INPUT(i-1,j) +
+	                                   INPUT(i,j-1)   + 2 * INPUT(i,j)   +
+	                                   INPUT(i+1,j-1) + 2 * INPUT(i+1,j) )/9;
 	                    }
 	                    else if (i == midIndex){
-	                    	temp2 = (PINPUT(i-1,j-1) + PINPUT(i-1,j) + PINPUT(i-1,j+1) +
-	                                   2 *PINPUT(i,j-1)   + 2 *PINPUT(i,j)   + 2 *PINPUT(i,j+1) )/9;
+	                    	temp2 = (INPUT(i-1,j-1) + INPUT(i-1,j) + INPUT(i-1,j+1) +
+	                                   2 *INPUT(i,j-1)   + 2 *INPUT(i,j)   + 2 *INPUT(i,j+1) )/9;
 	                    }
 	                    else {
-		                    temp2 = (PINPUT(i-1,j-1) + PINPUT(i-1,j) + PINPUT(i-1,j+1) +
-		                                   PINPUT(i,j-1)   + PINPUT(i,j)   + PINPUT(i,j+1)   +
-		                                   PINPUT(i+1,j-1) + PINPUT(i+1,j) + PINPUT(i+1,j+1) )/9;                    	
+		                    temp2 = (INPUT(i-1,j-1) + INPUT(i-1,j) + INPUT(i-1,j+1) +
+		                                   INPUT(i,j-1)   + INPUT(i,j)   + INPUT(i,j+1)   +
+		                                   INPUT(i+1,j-1) + INPUT(i+1,j) + INPUT(i+1,j+1) )/9;                    	
 	                    }
 
-	                     POUTPUT(i,j) = temp2;
+	                     OUTPUT(i,j) = temp2;
 
 
 
@@ -70,24 +57,27 @@ void simulate(double *input, double *output, int threads, int length, int iterat
 
     	}
 
-        if (n != iterations -1){
-                temp = pInput;
-                pInput = pOutput;
-                pOutput = temp;
-            }    
+        temp = input;
+        input = output;
+        output = temp;   
     }
 
+
+    temp = input;
+    input = output;
+    output = temp;
 
     
     for(int i = 0 ; i < length/2 ; ++i){
         for(int j = 0 ; j < length/2 ; ++j){
-            double cur = POUTPUT(i, j);
-            //OUTPUT(i,j) = cur;
-            //OUTPUT(length-1-i, j) = cur;
-            //OUTPUT(i, length-1-j) = cur;
-            //OUTPUT(length-1-i, length-1-j) = cur;
+            double cur = OUTPUT(i, j);
+            OUTPUT(i,j) = cur;
+            OUTPUT(length-1-i, j) = cur;
+            OUTPUT(i, length-1-j) = cur;
+            OUTPUT(length-1-i, length-1-j) = cur;
         }
     }
+
 
     
 }
