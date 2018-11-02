fbbb633636b4e89c318eb3005bc285a051eb185f
Simon Wicky <simon.wicky@epfl.ch>
2018-11-02 09:46:30 +0100

    Auto stash before merge of "master" and "origin/master"

--- Assignement 2/algorithm.c
+++ Assignement 2/algorithm.c
@@ -35,21 +35,38 @@ void simulate(double *input, double *output, int threads, int length, int iterat
 	        for(int i=1; i<length / 2; i++) {
 	            
 	            
-	            for(int j=1; j<length / 2; j++) {
+	            for(int j=i; j<length / 2; j++) {
 
 	                    if ( (i == midIndex) && (j == midIndex)){
 	                        continue;
 	                    }
 	                    double temp2;
 
-	                    if (j == midIndex){
+	                    //diagonal
+	                    if (i == j){
+		                    temp2 = (INPUT(i-1,j-1) + 2 * INPUT(i-1,j) + 2 * INPUT(i-1,j+1) +
+                                   	 					INPUT(i,j)   + 2 * INPUT(i,j+1)   +
+                               												INPUT(i+1,j+1) )/9;
+	                    }
+
+	                    //special case above diagonal in central column
+	                    else if (j == midIndex && j ==  i + 1){
+		                    temp2 = (INPUT(i-1,j-1) + 2 * INPUT(i-1,j) +
+                                   INPUT(i,j-1)   + 3 * INPUT(i,j)   +
+                                   					2 * INPUT(i+1,j) )/9;   
+	                    }
+	                    //above diagonal
+	                    else if (j == i + 1){
+		                    temp2 = (INPUT(i-1,j-1) + INPUT(i-1,j) + INPUT(i-1,j+1) +
+		                                   INPUT(i,j-1)   + 2 * INPUT(i,j)   + INPUT(i,j+1)   +
+		                                   					 INPUT(i+1,j) + INPUT(i+1,j+1) )/9;  	                    	
+	                    }
+
+	                    //central column
+	                    else if (j == midIndex){
 	                    	temp2 = (INPUT(i-1,j-1) + 2 * INPUT(i-1,j) +
 	                                   INPUT(i,j-1)   + 2 * INPUT(i,j)   +
 	                                   INPUT(i+1,j-1) + 2 * INPUT(i+1,j) )/9;
-	                    }
-	                    else if (i == midIndex){
-	                    	temp2 = (INPUT(i-1,j-1) + INPUT(i-1,j) + INPUT(i-1,j+1) +
-	                                   2 *INPUT(i,j-1)   + 2 *INPUT(i,j)   + 2 *INPUT(i,j+1) )/9;
 	                    }
 	                    else {
 		                    temp2 = (INPUT(i-1,j-1) + INPUT(i-1,j) + INPUT(i-1,j+1) +
@@ -71,15 +88,29 @@ void simulate(double *input, double *output, int threads, int length, int iterat
         output = temp;   
     }
 
+    //output = input;
+    //temp = input;
+    //output = temp;
 
     #pragma omp parallel for
     for(int i = 0 ; i < length/2 ; ++i){
-        for(int j = 0 ; j < length/2 ; ++j){
+        for(int j = i ; j < length/2 ; ++j){
+            if ( (i == midIndex) && (j == midIndex)){
+            	continue;
+            }	
             double cur = OUTPUT(i, j);
+
             OUTPUT(i,j) = cur;
+            OUTPUT(j,i) = cur;
+
             OUTPUT(length-1-i, j) = cur;
+            OUTPUT(j, length-1-i) = cur;
+
             OUTPUT(i, length-1-j) = cur;
+            OUTPUT(length-1-j, i) = cur;
+
             OUTPUT(length-1-i, length-1-j) = cur;
+            OUTPUT(length-1-j, length-1-i) = cur;
         }
     }
     free(pInput);
