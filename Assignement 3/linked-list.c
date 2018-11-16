78a018173a044b823150bbb009d441e3f93609f0
Simon Wicky <simon.wicky@epfl.ch>
2018-11-16 16:13:16 +0100

    answer question 4 and 5 by implementing item lock on list

--- Assignement 3/linked-list.c
+++ Assignement 3/linked-list.c
@@ -3,21 +3,29 @@ typedef struct node {
     int val;
     struct node *next;
     int to_remove;
+    omp_lock_t lock;
 } node_t;
 
 /* This function inserts a new given element at the right position of a given linked list.
  * It returns 0 on a successful insertion, and -1 if the list already has that value.
  */
 int insert(node_t *head, int val) {
-    node_t *previous, *current;
+    node_t *previous, *current = NULL;
     current = head;
+    omp_set_lock(&current->lock);
 
     while (current && current->val < val) {
+        if (previous){
+            omp_unset_lock(&previous->lock);
+        }
         previous = current;
         current  = current->next;
+        omp_set_lock(&current->lock);
     }
 
     if (current && current->val == val) { // This value already exists!
+        omp_unset_lock(&previous->lock);
+        omp_unset_lock(&current->lock);
         return -1;
     }
     
@@ -27,8 +35,11 @@ int insert(node_t *head, int val) {
     new_node->val = val;
     new_node->next = current;
     new_node->to_remove = 0;
+    omp_init_lock(&new_node->lock);
 
     previous->next = new_node;
+    omp_unset_lock(&previous->lock);
+    omp_unset_lock(&current->lock);
     return 0;
 }
 
@@ -44,16 +55,24 @@ int delete(node_t *head, int val) {
 
     previous = head;
     current = head->next;
+    omp_set_lock(&previous->lock);
+    omp_set_lock(&current->lock);
     while (current) {
         if (current->val == val) {
             previous->next = current->next;
             current->to_remove = 1; // Another system component will free this node later
+            omp_unset_lock(&previous->lock);
+            omp_unset_lock(&current->lock);
             return val;
         }
-
+        omp_unset_lock(&previous->lock);
         previous = current;
         current  = current->next;
+        omp_set_lock(&current->lock);
+
     }
+    omp_unset_lock(&previous->lock);
+    omp_unset_lock(&current->lock);
     return -1;
 }
 
