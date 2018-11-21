c0e21793ec96e42f7e000e47d136f270c4ff9431
samuelchassot <samuel.chassot@epfl.ch>
2018-11-21 11:58:57 +0100

    add insert for traversal without locking

--- Assignement 3/linked-list-fresh.c
+++ Assignement 3/linked-list-fresh.c
@@ -3,6 +3,7 @@ typedef struct node {
     int val;
     struct node *next;
     int to_remove;
+    omp_lock_t lock;
 } node_t;
 
 /* This function inserts a new given element at the right position of a given linked list.
@@ -13,10 +14,8 @@ int insert(node_t *head, int val) {
     current = head;
 
     while (current && current->val < val) {
-        #pragma omp atomic{ 
-            previous = current;
-            current  = current->next;
-        }
+        previous = current;
+        current  = current->next;
     }
 
     if (current && current->val == val) { // This value already exists!
@@ -24,17 +23,28 @@ int insert(node_t *head, int val) {
     }
     
     // Here is the right position to insert the new node.
+
+    
+    if(previous->next == current && !(current->lock) && !(previous->lock)){
+        #pragma omp atomic{
+            omp_set_lock(&current->lock);
+            omp_set_lock(&previous->lock);
+        }
+    } else {
+        insert(head, val);
+    }
+    
     node_t *new_node;
     new_node = malloc(sizeof(node_t));
     new_node->val = val;
+    new_node->next = current;
     new_node->to_remove = 0
-    
-    #pragma omp atomic{
-        current = previous->next;
-        new_node->next = current;
-        previous->next = new_node;
-    }
+    omp_init_lock(new_node->lock)
 
+    previous->next = new_node;
+
+    omp_unset_lock(&current->lock);
+    omp_unset_lock(&previous->lock);
     return 0;
 }
 
@@ -48,23 +58,17 @@ int delete(node_t *head, int val) {
         return -1;
     }
 
-    #pragma omp atomic{
-        previous = head;
-        current = head->next;
-    }
-
+    previous = head;
+    current = head->next;
     while (current) {
         if (current->val == val) {
-            #pragma omp atomic{
-                previous->next = current->next;
-                current->to_remove = 1; // Another system component will free this node later
-            }
+            previous->next = current->next;
+            current->to_remove = 1; // Another system component will free this node later
             return val;
         }
-        #pragma omp atomic{
-            previous = current;
-            current  = current->next;
-        }
+
+        previous = current;
+        current  = current->next;
     }
     return -1;
 }
@@ -80,7 +84,6 @@ int search(node_t *head, int val) {
         if (current->val == val) {
             return 0;
         }
-        #pragma omp atomic
         current  = current->next;
     }
 
