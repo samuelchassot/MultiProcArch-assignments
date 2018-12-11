a2d307497f48224263a1e36bb7db5030c0434e08
Simon Wicky <simon.wicky@epfl.ch>
2018-12-11 19:17:22 +0100

    pose the base of the algo

--- Assignement 4/implementation.cu
+++ Assignement 4/implementation.cu
@@ -46,6 +46,38 @@ void array_process(double *input, double *output, int length, int iterations)
     }
 }
 
+__global__ void array_process_GPU(double *input, double *output, int length, int iterations){
+    double *temp;
+
+    for(int n=0; n<(int) iterations; n++)
+    {
+        for(int i=1; i<length-1; i++)
+        {
+            for(int j=1; j<length-1; j++)
+            {
+                output[(i)*(length)+(j)] = (input[(i-1)*(length)+(j-1)] +
+                                            input[(i-1)*(length)+(j)]   +
+                                            input[(i-1)*(length)+(j+1)] +
+                                            input[(i)*(length)+(j-1)]   +
+                                            input[(i)*(length)+(j)]     +
+                                            input[(i)*(length)+(j+1)]   +
+                                            input[(i+1)*(length)+(j-1)] +
+                                            input[(i+1)*(length)+(j)]   +
+                                            input[(i+1)*(length)+(j+1)] ) / 9;
+
+            }
+        }
+        output[(length/2-1)*length+(length/2-1)] = 1000;
+        output[(length/2)*length+(length/2-1)]   = 1000;
+        output[(length/2-1)*length+(length/2)]   = 1000;
+        output[(length/2)*length+(length/2)]     = 1000;
+
+        temp = input;
+        input = output;
+        output = temp;
+    }
+}
+
 
 // GPU Optimized function
 void GPU_array_process(double *input, double *output, int length, int iterations)
@@ -60,24 +92,44 @@ void GPU_array_process(double *input, double *output, int length, int iterations
     cudaEventCreate(&comp_end);
 
     /* Preprocessing goes here */
+    double* input_GPU;
+    double* output_GPU;
+    int size = length * length;
+    cudaMalloc((void**) &input_GPU, size);
+    cudaMalloc((void**) &output_GPU, size);
 
     cudaEventRecord(cpy_H2D_start);
+
+
     /* Copying array from host to device goes here */
+
+    cudaMemcpy(input_GPU, input, size, cudaMemcpyHostToDevice);
     cudaEventRecord(cpy_H2D_end);
     cudaEventSynchronize(cpy_H2D_end);
 
     //Copy array from host to device
+
+
     cudaEventRecord(comp_start);
     /* GPU calculation goes here */
+    array_process_GPU <<< 32,128 >>> (input_GPU, output_GPU, length, iterations);
+
+
     cudaEventRecord(comp_end);
     cudaEventSynchronize(comp_end);
 
     cudaEventRecord(cpy_D2H_start);
+
+
     /* Copying array from device to host goes here */
+    cudaMemcpy(output_GPU, output, size, cudaMemcpyDeviceToHost);
+
     cudaEventRecord(cpy_D2H_end);
     cudaEventSynchronize(cpy_D2H_end);
 
     /* Postprocessing goes here */
+
+
 
     float time;
     cudaEventElapsedTime(&time, cpy_H2D_start, cpy_H2D_end);
