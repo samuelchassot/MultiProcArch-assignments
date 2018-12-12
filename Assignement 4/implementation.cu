c761fe89ad052815d0e1e73af04f148ef9e401fd
Simon Wicky <simon.wicky@epfl.ch>
2018-12-12 14:08:46 +0100

    b

--- Assignement 4/implementation.cu
+++ Assignement 4/implementation.cu
@@ -50,8 +50,8 @@ void array_process(double *input, double *output, int length, int iterations)
 }
 
 __global__ void array_process_GPU(double *input, double *output, int length){
-    int x = (blockIdx.x * blockDim.x) + threadIdx.x;
-    int y = (blockIdx.y * blockDim.y) + threadIdx.y;
+    int x = blockIdx.x;
+    int y = threadIdx.x;
         OUTPUT(0,0) = x;
         OUTPUT(0,1) = y;
     if(y > 0 && y < length - 1 && x > 0 && x < length - 1 ){
