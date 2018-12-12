11ef42adb4baeaa7012ffd22bcdea523ee54fe65
samuelchassot <samuel.chassot@epfl.ch>
2018-12-12 15:21:50 +0100

    replace execute.sh with the new

--- Assignement 4/execute.sh
+++ Assignement 4/execute.sh
@@ -3,6 +3,12 @@
 #SBATCH --partition=gpu
 #SBATCH --qos=gpu_free
 #SBATCH --gres=gpu:1
+#SBATCH --partition=gpu
+#SBATCH --account cs307
+#SBATCH --nodes=1
+#SBATCH --time=1:0:0
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
 
 length=50
 iterations=1
